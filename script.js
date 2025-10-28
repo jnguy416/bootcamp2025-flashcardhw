ca7dfe9249a3a91b7655cd9b9f65920636c60008
Jasmine Nguyen <jasminenguyen@Jasmines-MacBook-Pro-3.local>
2025-10-27 20:33:13 -0400

    completed display functionality

--- script.js
+++ script.js
@@ -12,10 +12,34 @@ let showingTerm = true;
 
 // Start with this function to simply display the card
 function displayCard() {
-
+    const card = document.getElementById("card-content");
+    card.innerText = flashcards[currentIndex].term;
+    showingTerm = false;
 }
 
+function displayAnswer() {
+    const card = document.getElementById("card-content");
+    card.innerText = flashcards[currentIndex].definition;   
+    showingTerm = true;
+}
+
+// displays first card in the flashcards array
+displayCard();
+
 // The rest of the code you will write is apart of event listeners
+
+// if the definition is showing, display the term... otherwise, display the answer then change showingTerm appropriately
+const card = document.querySelector(".flashcard")
+card.addEventListener("click", () => { 
+        if (showingTerm) {
+            showingTerm = !showingTerm;
+            displayCard();
+        } else {
+            showingTerm = !showingTerm;
+            displayAnswer();
+        }
+    }
+);
 
 // This line will display the card when the page is refreshed
 window.onload = displayCard;
